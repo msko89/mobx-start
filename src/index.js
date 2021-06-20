15ebe8ddc9e3f977d72b899a59044b4a046b09f4
msko <kms89123@gmail.com>
2021-06-20 23:31:43 +0900

    @observable mobx

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { autorun, makeObservable, observable } from 'mobx';
+
+const isLogin = observable(true);
+const person = observable({
+  name: 'msko',
+  age: 33,
+});
+
+class PersonStore {
+  @observable
+  name = 'msko';
+
+  @observable
+  age = 33;
+
+  constructor() {
+    makeObservable(this);
+  }
+}
+
+const personStore = new PersonStore();
+
+autorun(() => {
+  console.log(isLogin.get());
+  console.log(person.age);
+  console.log(personStore.age);
+});
+
+isLogin.set(false);
+person.age = 30;
+
+personStore.age = 40;
 
 ReactDOM.render(
   <React.StrictMode>
